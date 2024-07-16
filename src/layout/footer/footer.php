eee1e790b3b3f68a816758b4780ad57fd0b982a3
tomengsanchez <mrtomy31@gmail.com>
2024-07-16 10:59:13 +0800

    sample commit

--- src/layout/footer/footer.php
+++ src/layout/footer/footer.php
@@ -6,7 +6,7 @@ function shell_footer()
     </div>
     <div class="text-center py-3 mx-2" style="background-color: #21D192">
         © <?php echo date('Y')?> Copyright:
-        <a class="text-dark" href="#">Ecosys Corp</a>
+        <a class="text-dark" href="#">Ecosys Corp PH</a>
     </div>
 
 
