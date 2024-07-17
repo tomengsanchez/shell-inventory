e040f0da4d254e389b1e121597d39f6df61d7738
tomengsanchez <mrtomy31@gmail.com>
2024-07-17 14:58:30 +0800

    tomeng:fixed bootstrap wp_enqueue to javascript

--- src/layout/bootstrap/bootstrap.php
+++ src/layout/bootstrap/bootstrap.php
@@ -2,7 +2,7 @@
 $_GET['page'] = $_GET['page'] ?? null;
  if(count(explode('shell',$_GET['page'])) > 1){
     wp_enqueue_style( 'shell-bootstrap-css', plugin_dir_url( __FILE__ ) . '/css/bootstrap.css', []);
-    wp_enqueue_style( 'shell-bootstrap-js', plugin_dir_url( __FILE__ ) . '/js/bootstrap.bundle.min.js', []);
+    wp_enqueue_script( 'shell-bootstrap-js', plugin_dir_url( __FILE__ ) . '/js/bootstrap.bundle.min.js', []);
     
 
     
