25fa4d06b95fb624d988329296cca7eb0331d3cb
elaineorgasan <elaine.orgasan08@gmail.com>
2024-07-18 14:47:46 +0800

    dashboardupdate

--- src/modules/dashboard/presentation/dashboard.php
+++ src/modules/dashboard/presentation/dashboard.php
@@ -7,14 +7,17 @@ function shell_dashboard(){
     
    
    <div class="page">
-    <header class="navbar navbar-expand-md d-print-none">
-        <div class="container-fluid">
+    <header class="navbar navbar-expand-md d-print-none bg-light">
+        <div class="container">
          <h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
-         Shell Inventory
+          <a href="http://www.shell.com.ph">
+          <img class="card-img-top" src="shell.jpg" idth="110" height="32" alt="Shell Inventory">
+            
+          </a>
          <h1>
             <div class="navbar-nav flex-row order-md-last">
                 <div class="d-none d-md-flex">
-                  <div class="nav-item dropdown d-none d-md-flex me-3">
+                  <div class="nav-item dropdown d-none d-md-flex ">
                     <a class="nav-link px-0" href="#" data-bs-toggle="dropdown" tabindex="1" aria-label="show notification">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
                     <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6"/>
@@ -24,16 +27,17 @@ function shell_dashboard(){
                 </div>
             </div>
         </div>
-    
+           
     </header>
 
-    <header class="navbar-expand-md">
-        <div id="navbar-menu " class="collapse navbar-collapse">
+    <header class="navbar navbar-expand-lg bg-light">
+      <div class="container">
+        <div id="navbar-menu  " class="collapse navbar-collapse">
             <div class="navbar">
                 <div class="container-xl">
                     <ul class="navbar-nav">
                         <li class="nav-item active">
-                            <a class="nav-link" href="#">
+                            <a class="nav-link me-2" href="#">
                             <span class="nav-link-icon d-md-none d-lg-inline-block">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-house-door me-1" viewBox="0 0 16 16">
                                 <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4z"/>
@@ -45,7 +49,7 @@ function shell_dashboard(){
                             </a>
                         </li>
 
-                        <li class="nav-item me-3">
+                        <li class="nav-item me-2">
                             <a class="nav-link" href="https://dev.ecosyscorp.com/elaine/Products.html">
                             <span class="nav-link-icon d-md-none d-lg-inline-block">
                                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-boxes me-1" viewBox="0 0 16 16">
@@ -58,7 +62,7 @@ function shell_dashboard(){
                             </a>
                         </li>
 
-                        <li class="nav-item dropdown me-3">
+                        <li class="nav-item dropdown me-2">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart3 me-1" viewBox="0 0 16 16">
                             <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
@@ -71,7 +75,7 @@ function shell_dashboard(){
                             </ul>
                         </li>
 
-                        <li class="nav-item dropdown me-3">
+                        <li class="nav-item dropdown me-2">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box me-1" viewBox="0 0 16 16">
                             <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5 8 5.961 14.154 3.5zM15 4.239l-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464z"/>
@@ -83,7 +87,7 @@ function shell_dashboard(){
                             </ul>
                         </li> 
 
-                        <li class="nav-item me-3">
+                        <li class="nav-item me-2">
                             <a class="nav-link" href="https://dev.ecosyscorp.com/elaine/Products.html">
                             <span class="nav-link-icon d-md-none d-lg-inline-block">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-file-earmark me-1" viewBox="0 0 16 16">
@@ -96,7 +100,7 @@ function shell_dashboard(){
                             </a>
                         </li>
 
-                        <li class="nav-item dropdown me-3">
+                        <li class="nav-item dropdown me-4">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20  " fill="currentColor" class="bi bi-gear me-2" viewBox="0 0 16 16">
                             <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492M5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0"/>
@@ -110,47 +114,55 @@ function shell_dashboard(){
                     </ul>
                 </div>
             </div>
+          </div>
+          <form class="d-flex">
+            <input class="input-text me-1" type= "search" placeholder="Search.. " aria-label="Search">
+           </form>
         </div> 
     </header>
 </div>
-      
 
 <div class="page-body ">
-<div class="col">
-<div class="page-pretitle">Overview
-</div>
-<h5 class="page-title"><strong>Dashboard</strong>
-  </h5>
-    </div>
-
-        <div class="col-auto ms-auto d-print-none">
-            <div class="btn-list">
-              <a class="btn btn-secondary btn-sm d-none d-sm-inline-block" href="http://dev.ecosyscorp.com/elaine/orders/create.html">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg mt-1.7  " viewBox="0 0 16 16">
+<div class="container py-4">
+<div class="page-header d-print-none">
+  <div class="container-xl">
+    <div class="row g-2 align-items-center">
+      <div class="col py-2">
+        <div class="page-pretitle">
+          Overview
+        </div>
+        <h2 class="page-title">Dashboard</h2>
+      </div>
+      <!-- Page title action -->
+       <div class="col-auto ms-auto d-print-none">
+        <div class="btn-list">
+          <a class=" btn btn-danger d-none d-sm-inline-block" href="https://inventory.ecosyscorp.ph/orders/create">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg mt-1.7  " viewBox="0 0 16 16">
                   <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
                   <path stroke="none" d="M0 0h24v2H0z" fill="none"></path>
                   <path d="M12 5l0 14"></path>
                   <path d="M5 12l14 0"></path>
-                </svg>
-                Create new orders
-              </a>
+          </svg>
+          Create new order
+          </a>
             </div>
-           </div>
+          </div>
         </div>
       </div>
   </div>
+</div>
 
       <div class="container-xl">
         <div class="row row-deck row-cards">
           <div class="col-12">
             <div class="row row-cards">
               <div class="col-sm-6 col-lg-3">
-                <div class="card card-sm">
+                <div class="card card-sm ">
                   <div class="card-body">
                     <div class="row align-items-center">
                       <div class="col-auto">
                         <span class="text-white avatar">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="Dark" class="bi bi-boxes" viewBox="0 0 16 16">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="Dark" class="bi bi-boxes " viewBox="0 0 16 16">
                           <path d="M7.752.066a.5.5 0 0 1 .496 0l3.75 2.143a.5.5 0 0 1 .252.434v3.995l3.498 2A.5.5 0 0 1 16 9.07v4.286a.5.5 0 0 1-.252.434l-3.75 2.143a.5.5 0 0 1-.496 0l-3.502-2-3.502 2.001a.5.5 0 0 1-.496 0l-3.75-2.143A.5.5 0 0 1 0 13.357V9.071a.5.5 0 0 1 .252-.434L3.75 6.638V2.643a.5.5 0 0 1 .252-.434zM4.25 7.504 1.508 9.071l2.742 1.567 2.742-1.567zM7.5 9.933l-2.75 1.571v3.134l2.75-1.571zm1 3.134 2.75 1.571v-3.134L8.5 9.933zm.508-3.996 2.742 1.567 2.742-1.567-2.742-1.567zm2.242-2.433V3.504L8.5 5.076V8.21zM7.5 8.21V5.076L4.75 3.504v3.134zM5.258 2.643 8 4.21l2.742-1.567L8 1.076zM15 9.933l-2.75 1.571v3.134L15 13.067zM3.75 14.638v-3.134L1 9.933v3.134z"/>
                           </svg>
                         </span>
@@ -169,12 +181,12 @@ function shell_dashboard(){
               </div>
 
               <div class="col-sm-6 col-lg-3">
-                <div class="card card-sm">
+                <div class="card card-sm ">
                   <div class="card-body">
                     <div class="row align-items-center">
                       <div class="col-auto">
                        <span class="text-white avatar">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="Dark" class="bi bi-cart3" viewBox="0 0 16 16">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="Dark" class="bi bi-cart3 " viewBox="0 0 16 16">
                           <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
                           </svg>
                         </span>
@@ -193,12 +205,12 @@ function shell_dashboard(){
               </div>
 
               <div class="col-sm-6 col-lg-3">
-                <div class="card card-sm">
+                <div class="card card-sm bg-muted">
                   <div class="card-body">
                     <div class="row align-items-center">
                       <div class="col-auto">
                         <span class="text-white avatar">
-                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="Dark" class="bi bi-box" viewBox="0 0 16 16">
+                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="Dark" class="bi bi-box " viewBox="0 0 16 16">
                          <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5 8 5.961 14.154 3.5zM15 4.239l-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464z"/>
                          </svg>
                         </span>
@@ -217,12 +229,12 @@ function shell_dashboard(){
               </div>
 
               <div class="col-sm-6 col-lg-3">
-                <div class="card card-sm">
+                <div class="card card-sm ">
                   <div class="card-body">
                     <div class="row align-items-center">
                       <div class="col-auto">
                         <span class="text-white avatar">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="Dark" class="bi bi-file-earmark" viewBox="0 0 16 16">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="Dark" class="bi bi-file-earmark " viewBox="0 0 16 16">
                             <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5z"/>
                           </svg>
                         </span>
